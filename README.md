b15fc9a1cced2e55d2b168130f38116326947294
Vitali64 <5405891-vitali64yt@users.noreply.gitlab.com>
2021-10-28 14:34:47 +0200

     add build script and modify readme.md

--- README.md
+++ README.md
@@ -9,4 +9,22 @@ Links :
 - [Original README](./ORIGINALREADME.md)
 - [Patches used](./patches/)
 
+### Build
+
+For building the rom, you can do it [the official way](https://doc.coreboot.org) but it's recommended to use the script provided in the repo :
+
+	./build.sh <distro> <option>
+
+For example, if you use Ubuntu and want to save logs, you would run :
+
+	user@host:/path/of/coreboot$ ./build.sh debian log
+
+for more information about how to use the script:
+
+	user@host:/path/of/coreboot$ ./build.sh help
+
+### Issues
+
+If you ran into an issue or bug, before reporting it, please read the [following](https://github.com/Vitali64GitHub/coreboot-macbook21-mod/issues/1)
+
 
---
+++ build.sh
@@ -0,0 +1,135 @@
+#!/bin/bash
+
+usage() {
+	echo "Usage : ./build.sh <distro> <options>"
+	echo "# Distros compatible :"
+	echo "Arch-based/ManJaro-based : arch"
+	echo "Debian-based/Ubuntu-based : debian"
+	echo "Fedora-based/RedHat-based/CentOS-based : fedora"
+	echo "# Options available :"
+	echo "Save logs (useful for reporting) : log"
+	echo "Do not use 'clear' : noclr"
+}
+
+if [ "$1" == "arch" ]; then
+	distro="arch"
+elif [ "$1" == "debian" ]; then
+	distro="debian"
+elif [ "$1" == "fedora" ]; then
+	distro="fedora"
+elif [ "$1" == "help" ]; then
+	usage
+elif [ "$1" != "arch" ]; then
+	usage
+	echo "SCRIPT NEEDS ARGUMENTS"
+	exit 1
+elif [ $1 != "debian" ]; then
+	usage
+	echo "SCRIPT NEEDS ARGUMENTS"
+	exit 1
+elif [ $1 != "fedora" ]; then
+	usage
+	echo "SCRIPT NEEDS ARGUMENTS"
+	exit 1
+elif [ "$2" == "log" ]; then
+	log="true"
+	mkdir LOGS
+	echo "Logs enabled ! Logs are available in the 'LOGS/' directory"
+elif [ "$2" == "noclr" ]; then
+	alias clear="echo"
+fi
+
+
+error() {
+	echo "FATAL ERROR, PLEASE REPORT : $1" && exit 1
+}
+
+welcome() {
+	echo "Welcome to "
+	echo "This coreboot fork adding features not available upstream, to the MacBook!"
+	sleep 2
+}
+
+dependencies() {
+	clear
+	echo "Before we start, we need to download dependencies for coreboot ..."
+	sleep 1
+
+	if [ "$log" == "true" ]; then
+		if [ "$distro" == "arch" ]; then
+			sudo pacman --sync --needed --noconfirm base-devel curl git gcc-ada ncurses zlib > LOGS/Dependencies.log
+		elif [ "$distro" == "debian" ]; then
+			sudo apt-get install -y bison build-essential curl flex git gnat libncurses5-dev m4 zlib1g-dev > LOGS/Dependencies.log
+		else
+			sudo dnf install git make gcc-gnat flex bison xz bzip2 gcc g++ ncurses-devel wget zlib-devel > LOGS/Dependencies.log
+		fi
+	else
+		if [ "$distro" == "arch" ]; then
+			sudo pacman --sync --needed --noconfirm base-devel curl git gcc-ada ncurses zlib
+		elif [ "$distro" == "debian" ]; then
+			sudo apt-get install -y bison build-essential curl flex git gnat libncurses5-dev m4 zlib1g-dev
+		else
+			sudo dnf install git make gcc-gnat flex bison xz bzip2 gcc g++ ncurses-devel wget zlib-devel
+		fi
+	fi
+
+	if [ "$?" != 1 ]; then
+		echo "done!"
+	else
+		error "Failed to install dependencies !"
+	fi
+	sleep 1
+}
+
+toolchain() {
+	clear
+	echo "Now, let's build the coreboot toolchain ... This is gonna take a while ..."
+	sleep 1
+
+	if [ $log == "true" ]; then
+		make crossgcc-i386 CPUS="$(nproc)" > LOGS/Toolchain.log
+	else
+		make crossgcc-i386 CPUS="$(nproc)"
+	fi
+
+	if [ "$?" != 1 ]; then
+		echo "done!"
+	else
+		error "Failed to build toolchain !"
+	fi
+	sleep 1
+}
+
+build() {
+	clear
+	echo "Now, everything is ready, now, let's build the actual rom !"
+	sleep 1
+
+	if [ "$log" == "true" ]; then
+		make  > LOGS/Build.log
+	else
+		make
+	fi
+
+	if [ "$?" != 1 ]; then
+		echo "done!"
+	else
+		error "Failed to compile coreboot !"
+	fi
+}
+
+finish() {
+	echo "Your coreboot rom is now ready ! Enjoy your new firmware !"
+
+	echo "Exiting ..."
+	exit 0
+}
+
+# RUN ALL OF THIS
+
+welcome
+dependencies
+toolchain
+build
+finish
+
